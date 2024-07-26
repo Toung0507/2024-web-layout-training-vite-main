41de5598995c77f3bcf00d8c72faaebfd6470b50
Toung0507 <spexial110@gmail.com>
2024-07-26 22:37:06 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/2024_Hexo_figmatohtml_class_example_training-web-layout-training-vite-main/',
+  base: '/2024-web-layout-training-vite-main/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
